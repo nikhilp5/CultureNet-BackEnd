cbbb49fdfe329087c540d5a10a55c389f2278ea3
Monil Andharia <andhariamonil@dal.ca>
2023-03-27 16:50:53 -0300

    Added database

---
+++ functions/models/books/books.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('../utils/dbConn');
+
+const booksModel = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  authors: {
+    type: [String],
+    required: true,
+    trim: true,
+  },
+  dateReleased: {
+    type: Date,
+  },
+  image: {
+    type: String,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model('books', booksModel);
---
+++ functions/models/users.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('../utils/dbConn');
+
+const usersModel = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    trim: true,
+    default: '',
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    default: '',
+  },
+  bio: {
+    type: String,
+    trim: true,
+    default: '',
+  },
+  nsfw: {
+    type: Boolean,
+    default: false,
+  },
+  code: {
+    type: String,
+    trim: true,
+  },
+  codeExpiry: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model('users', usersModel);
