5b1edb058be957ca53a085dd1fc01e12bcb8293e
Monil Andharia <andhariamonil@dal.ca>
2023-04-02 04:34:09 -0300

    Added hardcoded JWT Secret Key

--- functions/utils/jwt.js
+++ functions/utils/jwt.js
@@ -4,13 +4,9 @@ var jwt = require('jsonwebtoken');
 const generateJWT = async (expiresIn, email) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const token = await jwt.sign(
-        { email },
-        process.env.JWT_SECRET_KEY || 'CultureNet@123',
-        {
-          expiresIn,
-        },
-      );
+      const token = await jwt.sign({ email }, 'CultureNet@123', {
+        expiresIn,
+      });
       resolve(token);
     } catch (error) {
       reject(error);
@@ -21,17 +17,13 @@ const generateJWT = async (expiresIn, email) => {
 const verifyJWT = (token) => {
   return new Promise((resolve, reject) => {
     try {
-      jwt.verify(
-        token,
-        process.env.JWT_SECRET_KEY || 'CultureNet@123',
-        (err, decoded) => {
-          if (err) {
-            resolve({ verify: false, data: null });
-          } else {
-            resolve({ verify: true, data: decoded.email });
-          }
-        },
-      );
+      jwt.verify(token, 'CultureNet@123', (err, decoded) => {
+        if (err) {
+          resolve({ verify: false, data: null });
+        } else {
+          resolve({ verify: true, data: decoded.email });
+        }
+      });
     } catch (error) {
       console.log(error);
       reject(error);
