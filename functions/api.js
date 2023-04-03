1bf7b72235390a8cb8f00cf4ca4e52afc5aaf17e
Monil Andharia <andhariamonil@dal.ca>
2023-04-03 15:49:05 -0300

    Added auth as middleware

--- functions/api.js
+++ functions/api.js
@@ -2,14 +2,16 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const routes = require('./routes/route');
+const userRoute = require('./routes/userRoute');
 const errorHandler = require('./utils/errorHandler');
+const jsonParser = bodyParser.json();
 
 app.use(cors());
-
-app.use('/.netlify/functions/api', routes);
+app.use(jsonParser);
+app.use('/.netlify/functions/api', userRoute);
 app.use('/.netlify/functions/api', errorHandler);
 
 module.exports = app;
