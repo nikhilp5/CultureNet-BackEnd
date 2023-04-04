8ce0926f3c0a7016dced45dfa935d6ac9cdd9442
Nikhil Sugathan Panikkassery <nk561034@dal.ca>
2023-04-03 23:19:47 -0300

    user.js updated

--- functions/contollers/user.js
+++ functions/contollers/user.js
@@ -1,37 +1,36 @@
-const validator = require("../utils/validator");
-const getError = require("../utils/getError");
-const User = require("../models/users.model");
+const validator = require('../utils/validator');
+const getError = require('../utils/getError');
+const User = require('../models/users.model');
+const sendEmail = require('../utils/mailer');
+const bcrypt = require('bcrypt');
+const jwtUtil = require('../utils/jwt');
 
-const fetchUserById = async (req, res, next) => {
-  try {
-    const targetRecord = await User.findById(req.params.id);
-    if (targetRecord) {
-      const { firstName, email } = targetRecord;
-      res.json({
-        success: true,
-        user: { firstName, email },
-      });
-    } else {
-      throw getError(404, "No such user found");
-    }
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
-};
+const HASH_ROUNDS = 15;
 
-const addUser = async (req, res, next) => {
+const register = async (req, res, next) => {
   try {
-    if (req.body && req.body.email && req.body.firstName) {
+    if (
+      req.body &&
+      req.body.email &&
+      req.body.password &&
+      req.body.confirmPassword
+    ) {
+      const { email, password, confirmPassword } = req.body;
+      if (password !== confirmPassword) {
+        throw getError(400, 'Passwords do not match');
+      }
       validator(req.body);
-      const { email, firstName } = req.body;
       const targetRecord = await User.findOne({ email });
       if (targetRecord) {
-        throw getError(409, "Email already exists");
+        throw getError(409, 'Email already exists');
+      }
+      const passwordHash = await bcrypt.hash(password, HASH_ROUNDS);
+      if (!passwordHash) {
+        throw getError(500, 'Error in password hashing');
       }
       const user = new User({
-        firstName,
         email,
+        password: passwordHash,
       });
 
       user
@@ -39,13 +38,13 @@ const addUser = async (req, res, next) => {
         .then((doc) => {
           console.log(doc);
           res.status(201);
-          res.json({ message: "User added", success: true });
+          res.json({ message: 'User added', success: true });
         })
         .catch((err) => {
           next(err);
         });
     } else {
-      throw getError(400, "Invalid or missing body paramaters");
+      throw getError(400, 'Invalid or missing body paramaters');
     }
   } catch (err) {
     console.log(err);
@@ -53,7 +52,225 @@ const addUser = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (req.body && req.body.email && req.body.password) {
+      const targetRecord = await User.findOne({
+        email,
+      });
+
+      if (targetRecord) {
+        const token = await jwtUtil.generateJWT('1h', targetRecord);
+        if (await bcrypt.compare(password, targetRecord.password)) {
+          res.json({
+            message: 'Login successful',
+            success: true,
+            token,
+            email,
+            id: targetRecord._id,
+          });
+        } else {
+          throw getError(401, 'Invalid credentials');
+        }
+      } else {
+        throw getError(404, 'User not found');
+      }
+    } else {
+      throw getError(400, 'Invalid or missing body paramaters');
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const forgotPassword = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (req.body && req.body.email) {
+      const code = Math.floor(100000 + Math.random() * 900000);
+
+      await sendEmail(
+        email,
+        `CultureNet - Password Reset Code - ${code}`,
+        'Enter the code below to reset the password:' + '\n' + code,
+      );
+
+      var user = await User.findOneAndUpdate(
+        { email },
+        { code, codeExpiry: Number(new Date()) },
+        {
+          new: true,
+        },
+      );
+      if (user) {
+        res.json({
+          message: 'Email sent',
+          success: true,
+        });
+      } else {
+        throw getError(404, 'User not found');
+      }
+    } else {
+      throw getError(400, 'Invalid or missing body paramaters');
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const resetPassword = async (req, res, next) => {
+  try {
+    if (req.body && req.body.code && req.body.email) {
+      const { email, code } = req.body;
+
+      const targetRecord = await User.findOne({
+        email,
+        code,
+      });
+
+      if (
+        targetRecord &&
+        Date.now() - Number(targetRecord.codeExpiry) < 600000
+      ) {
+        const token = await jwtUtil.generateJWT('10m', targetRecord);
+        res.json({
+          message: 'Code valid',
+          success: true,
+          token,
+        });
+      } else {
+        throw getError(401, 'Invalid or expired reset code');
+      }
+    } else {
+      throw getError(400, 'Invalid or missing body paramaters');
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const changePassword = async (req, res, next) => {
+  try {
+    if (req.body.email && req.body.password && req.body.confirmPassword) {
+      const { email, password, confirmPassword } = req.body;
+      if (password !== confirmPassword) {
+        throw getError(400, 'Passwords do not match');
+      }
+      validator(req.body);
+      const targetRecord = await User.findOne({ email });
+      if (!targetRecord) {
+        throw getError(404, 'User not found');
+      }
+      const passwordHash = await bcrypt.hash(password, HASH_ROUNDS);
+      if (!passwordHash) {
+        throw getError(500, 'Error in password hashing');
+      }
+      var user = await User.findOneAndUpdate(
+        { email },
+        { password: passwordHash },
+        {
+          new: true,
+        },
+      );
+      if (user) {
+        res.json({
+          message: 'User updated',
+          success: true,
+          user,
+        });
+      } else {
+        throw getError(404, 'User not found');
+      }
+    } else {
+      throw getError(400, 'Invalid or missing body paramaters');
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const getUserProfile = async (req, res, next) => {
+  try {
+    if (req.body && req.body.email) {
+      const { email } = req.body;
+      const user = await User.findOne({ email });
+      if (user) {
+        res.json({
+          message: 'User found',
+          success: true,
+          user,
+        });
+      } else {
+        throw getError(404, 'User not found');
+      }
+    } else {
+      throw getError(400, 'Invalid or missing body paramaters');
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const updateUserProfile = async (req, res, next) => {
+  try {
+    if (req.body && req.body.email) {
+      const { email, firstName, lastName, bio, nsfw } = req.body;
+      var user = await User.findOneAndUpdate(
+        { email },
+        { firstName, lastName, bio, nsfw },
+        {
+          new: true,
+        },
+      );
+      if (user) {
+        res.json({
+          message: 'User updated',
+          success: true,
+          user,
+        });
+      } else {
+        throw getError(404, 'User not found');
+      }
+    } else {
+      throw getError(400, 'Invalid or missing body paramaters');
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header('authorization');
+    if (!token) {
+      throw getError(401, 'User unauthorized');
+    }
+    token = token.split(' ')[1];
+    const isVerified = await jwtUtil.verifyJWT(token);
+    if (!isVerified.verify) {
+      throw getError(401, 'Token expired or invalid');
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
 module.exports = {
-  fetchUserById,
-  addUser,
+  register,
+  login,
+  forgotPassword,
+  changePassword,
+  getUserProfile,
+  updateUserProfile,
+  verifyToken,
+  resetPassword,
 };
