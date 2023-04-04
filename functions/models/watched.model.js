4dd34551776f64d0ef466299ccb3297598243780
Nikhil Sugathan Panikkassery <nk561034@dal.ca>
2023-04-04 20:32:29 -0300

    added watched backend

---
+++ functions/models/watched.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("../utils/dbConn");
+
+const watchedModel = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  bookId: {
+    type: [String],
+    required: true,
+    trim: true,
+  },
+  movieId: {
+    type: [String],
+    required: true,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("watcheds", watchedModel);
