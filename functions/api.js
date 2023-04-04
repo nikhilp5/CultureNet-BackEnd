148fee1478380a50f3487c2a7fe3c112609a6b5b
Nikhil Sugathan Panikkassery <nk561034@dal.ca>
2023-04-03 22:42:45 -0300

    updated api.js and dbconn

--- functions/api.js
+++ functions/api.js
@@ -5,10 +5,13 @@ const cors = require("cors");
 require("dotenv").config();
 
 const searchWatchlistRoute = require("./routes/searchWatchlistRoute");
+const userRoute = require('./routes/userRoute');
 const errorHandler = require("./utils/errorHandler");
 
 app.use(cors());
+app.use(jsonParser);
 app.use("/.netlify/functions/api", searchWatchlistRoute);
+app.use('/.netlify/functions/api', userRoute);
 app.use("/.netlify/functions/api", errorHandler);
 
 module.exports = app;
