9db6508735521cb543978161bcb1f9cd5ab0f64b
Ronil Patel <ronil.patel@dal.ca>
2023-04-06 21:55:35 -0300

    Updated Book Genres API

--- functions/routes/bookRoute.js
+++ functions/routes/bookRoute.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const { verifyToken } = require('../contollers/user');
 const bookController = require('../contollers/book');
+const bookGenreController = require('../contollers/bookGenre')
 
 router.get('/books', verifyToken, bookController.getAllBooks);
 router.get('/books/:id', verifyToken, bookController.getBookById);
+
+router.get('/bookgenre', verifyToken, bookGenreController.getAllBookGenres);
+router.get('/bookgenre/:id', verifyToken, bookGenreController.getBookGenreById);
 
 module.exports = router;
