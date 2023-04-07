aa4dcaa6cd77e5e871ff95a763d6a03018e53912
Ronil Patel <ronil.patel@dal.ca>
2023-04-06 23:03:42 -0300

    updated the Books api controller to add the filtered books

--- functions/contollers/book.js
+++ functions/contollers/book.js
@@ -4,10 +4,33 @@ const Book = require('../models/books/books.model');
 // Get all Books
 exports.getAllBooks = async (req, res) => {
     try {
-      const books = await Book.find();
-      res.json(books);
+        const { title, authors, genre } = req.query;
+        const books = await Book.find();
+        const filteredBooks = books;
+
+        // if (title) {
+        //     console.log("------------------------------------------");
+        //     filteredBooks = filteredBooks.filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
+        //     console.log("------------------------------------------");
+        //     console.log("Filtered books after query: " + filteredBooks);
+        // }
+        
+        // if (authors) {
+        //     filteredBooks = filteredBooks.filter(book => {
+        //       const bookAuthors = book.authors.map(author => author.toLowerCase());
+        //       const queryAuthors = authors.toLowerCase().split(',');
+        //       return queryAuthors.every(queryAuthor => bookAuthors.includes(queryAuthor));
+        //     });
+        //   }
+        
+        // if (genre) {
+        //     filteredBooks = filteredBooks.filter(book => book.genre.toLowerCase().includes(genre.toLowerCase()));
+        // }
+        
+        res.json(filteredBooks);
+
     } catch (error) {
-      res.status(400).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
   };
 
@@ -22,3 +45,5 @@ exports.getBookById = async (req, res) => {
       res.status(400).json({ message: error.message });
     }
   };
+
+
