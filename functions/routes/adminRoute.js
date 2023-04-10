2afd9bd33c6811d7235b212b640e2e8603cf1575
Rishi Vasa <rs310602@dal.ca>
2023-04-10 08:03:16 -0300

    Fixed Music Module

--- functions/routes/adminRoute.js
+++ functions/routes/adminRoute.js
@@ -10,7 +10,7 @@ const upload = multer();
 
 const { addMovie, updateMovie, deleteMovie } = require("../contollers/configureMovies");
 const { addBook, updateBook, deleteBook } = require("../contollers/configureBooks");
-//const { addMusic, updateMusic, deleteMusic } = require("../contollers/cofigureBooks");
+const { addMusic, updateMusic, deleteMusic } = require("../contollers/configureMusic");
 
 const { verifyToken } = require("../contollers/user");
 
@@ -20,9 +20,9 @@ adminRoute.route("/deleteMovie/:movieID").delete(verifyToken, deleteMovie);
 adminRoute.route("/addBook").post(upload.single('image'), verifyToken, addBook);
 adminRoute.route("/updateBook/:bookID").put(upload.single('image'), verifyToken, updateBook);
 adminRoute.route("/deleteBook/:bookID").delete(verifyToken, deleteBook);
-// adminRoute.route("/addMusic").post(verifyToken, addMusic);
-// adminRoute.route("/updateMusic/:musicID").put(verifyToken, updateMusic);
-// adminRoute.route("/deleteMusic/:musicID").delete(verifyToken, deleteMusic);
+adminRoute.route("/addMusic").post(verifyToken, addMusic);
+adminRoute.route("/updateMusic/:musicID").put(verifyToken, updateMusic);
+adminRoute.route("/deleteMusic/:musicID").delete(verifyToken, deleteMusic);
 
 module.exports = adminRoute;
  
